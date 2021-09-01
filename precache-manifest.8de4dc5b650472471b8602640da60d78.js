8628e45f4902b7d416205ef8d06166e11209122c
ayguid <guidoaimar@gmail.com>
2021-09-01 16:16:13 -0300

    gh-pages

---
+++ precache-manifest.8de4dc5b650472471b8602640da60d78.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0af25990e2eec2ba2bb",
+    "url": "/agile-test/css/about.270453dc.css"
+  },
+  {
+    "revision": "c9b6586474788769e560",
+    "url": "/agile-test/css/app.9b4bb80d.css"
+  },
+  {
+    "revision": "31e646771bd8f619364b",
+    "url": "/agile-test/css/chunk-vendors.f4e10491.css"
+  },
+  {
+    "revision": "0abd0a1f0649d6c9240c6c9c5b9235f7",
+    "url": "/agile-test/index.html"
+  },
+  {
+    "revision": "f0af25990e2eec2ba2bb",
+    "url": "/agile-test/js/about.a2314949.js"
+  },
+  {
+    "revision": "c9b6586474788769e560",
+    "url": "/agile-test/js/app.a9c1106d.js"
+  },
+  {
+    "revision": "31e646771bd8f619364b",
+    "url": "/agile-test/js/chunk-vendors.a60b5958.js"
+  },
+  {
+    "revision": "a3e0935792c1ace2ede480f325fd84c2",
+    "url": "/agile-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/agile-test/robots.txt"
+  }
+]);
